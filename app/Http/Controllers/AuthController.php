bb393d62a5bbdd86e673bfc53fce23325b2a3a82
kimkundad <kim.kundad@gmail.com>
2021-04-21 11:26:29 +0700

    no message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,7 @@ class AuthController extends Controller
 
      }
 
-     
+
 
     public function register(Request $request) {
        // dd($request->all());
@@ -118,7 +118,11 @@ class AuthController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function userProfile() {
-        return response()->json(auth()->user());
+        return response()->json([
+            'status'=>200,
+            'data' => auth()->user()
+        ]
+        );
     }
 
     public function get_tex_address(){
