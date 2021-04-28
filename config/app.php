622435954574730ac65a0ebc9529599b4ec31492
kimkundad <kim.kundad@gmail.com>
2021-04-29 05:41:19 +0700

    no message

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'url2' => 'http://localhost:3000',
+    'url2' => 'https://www.robotel.co.th',
 
     'asset_url' => env('ASSET_URL', null),
 
