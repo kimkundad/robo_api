005182626656acb7ccb97c8e6aaf17b7028ffe34
kimkunndad <kim.kundad@gmail.com>
2021-08-19 15:17:05 +0700

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -712,6 +712,33 @@ class AuthController extends Controller
 
         }
 
+
+
+            $message = "ชื่อบริษัท / ร้านค้า ".$request['name_company'].", ได้ทำการสมัคร ขอใช้งาน API เข้ามาใหม่";
+            $lineapi = env('line_token');
+            
+
+            $mms =  trim($message);
+            $chOne = curl_init();
+            curl_setopt($chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+            curl_setopt($chOne, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($chOne, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($chOne, CURLOPT_POST, 1);
+            curl_setopt($chOne, CURLOPT_POSTFIELDS, "message=$mms");
+            curl_setopt($chOne, CURLOPT_FOLLOWLOCATION, 1);
+            $headers = array('Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$lineapi.'',);
+            curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($chOne, CURLOPT_RETURNTRANSFER, 1);
+            $result = curl_exec($chOne);
+            if(curl_error($chOne)){
+            echo 'error:' . curl_error($chOne);
+            }else{
+            $result_ = json_decode($result, true);
+        //    echo "status : ".$result_['status'];
+        //    echo "message : ". $result_['message'];
+            }
+            curl_close($chOne);
+
         
 
         }  
