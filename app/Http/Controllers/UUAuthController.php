8d3fd849e9fb31d348577aed2f3ed4e530948fc2
kimkundad <kim.kundad@gmail.com>
2021-09-30 14:01:15 +0700

    kim

--- app/Http/Controllers/UUAuthController.php
+++ app/Http/Controllers/UUAuthController.php
@@ -11,20 +11,27 @@ class UUAuthController extends Controller
     //
     public function handleProviderCallback(Request $request){
 
-        return $request->all();
+        $response = Http::asForm()->post('https://siamtheatre.com/connect/token', [
+            'code' => $request['code'],
+            'scope' => $request['scope'],
+            'session_state' => $request['session_state'],
+        ]); 
+        
+
+        return $response->json();
         
     }
 
     public function redirectToProvider1(Request $request){
         
 
-        $request->session()->put('code_verifier', $code_verifier = Str::random(128));
+     /*   $request->session()->put('code_verifier', $code_verifier = Str::random(128));
 
         $codeChallenge = strtr(rtrim(
             base64_encode(hash('sha256', $code_verifier, true))
         , '='), '+/', '-_');
 
-        $response = Http::asForm()->post('https://api.robotel.co.th/oauth/robotel/callback', [
+        $response = Http::asForm()->post('https://siamtheatre.com/connect/authorize', [
             'redirect_uri' => 'https://oauth.pstmn.io/v1/callback',
             'client_id' => 'postman',
             'client_secret' => 'postman',
@@ -32,9 +39,11 @@ class UUAuthController extends Controller
             'code_challenge' => 'l0gl43mF9SzmCdttZQaKWKERf1VyRMC0CdPPbz1E8no',
             'code_challenge_method' => 'S256',
             'response_type' => 'code',
-        ]);
+        ]); */
         
-        return $response->json();
+       // return $response->json();
+
+       return view('home');
 
     }
 }
