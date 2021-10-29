eb64f6b4115533432ca559d449c38950f38e9e53
kimkundad <kim.kundad@gmail.com>
2021-10-29 10:22:52 +0700

    kim

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -96,7 +96,7 @@ class HomeController extends Controller
 
     public function get_qr_type(){
 
-        $cat = qr_code_type::where('status', 1)->Orderby('id', 'desc')->paginate(15);
+        $cat = qr_code_type::where('status', 1)->Orderby('id', 'asc')->paginate(15);
         return response()->json($cat);
         
     }
